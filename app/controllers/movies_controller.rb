128eae24934b87f08df0a4ed972de0435f5ae34b
EC2 Default User <ec2-user@ip-172-31-7-157.eu-west-1.compute.internal>
2019-02-20 16:43:51 +0000

    movie rating error

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,11 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-   
+    #@checked_ratings = check
+   # @checked_ratings.each do |rating|
+    #  params[rating] = true
+   # end
+
     @sort = params[:sort] || session[:sort] 
     session[:ratings] = session[:ratings] || {'G'=>'','PG'=>'','PG-13'=>'','R'=>''}
     @t_param = params[:ratings] || session[:ratings]
